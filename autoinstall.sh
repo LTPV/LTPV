8f613d5a7081bd6c1a0d6f55a1253ab267564b28
Francois-Xavier Lyonnet du Moutier <fx.du.moutier@gmail.com>
2014-04-06 00:30:03 +0200

    refactoring

--- autoinstall.sh
+++ autoinstall.sh
@@ -1,4 +1,4 @@
 #/usr/bin/bash
 sudo ln -fs $PWD/bin/ltpv /usr/bin/ltpv
 sudo ln -fs $PWD/build/libltpv.so /usr/lib/libltpv.so
-sudo ln -fs $PWD/header/ltpv.h /usr/include/ltpv.h
+sudo ln -fs $PWD/src/ltpv.h /usr/include/ltpv.h
